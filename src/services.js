7145b0f80223ed317938e97ebb9dc4c080d99e7e
Aaron <abarnard@protonmail.com>
2020-02-21 09:42:43 +1100

    Upgrade Onboard and add hardware wallets

--- src/services.js
+++ src/services.js
@@ -11,6 +11,18 @@ export function initOnboard(subscriptions) {
     walletSelect: {
       wallets: [
         { walletName: "metamask" },
+        {
+          walletName: "trezor",
+          appUrl: "https://reactdemo.blocknative.com",
+          email: "aaron@blocknative.com",
+          rpcUrl:
+            "https://rinkeby.infura.io/v3/d5e29c9b9a9d4116a7348113f57770a8"
+        },
+        {
+          walletName: "ledger",
+          rpcUrl:
+            "https://rinkeby.infura.io/v3/d5e29c9b9a9d4116a7348113f57770a8"
+        },
         { walletName: "dapper" },
         { walletName: "coinbase" },
         { walletName: "status" },
