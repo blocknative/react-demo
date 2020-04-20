1441f7bdc905798f2b326b1cde1c6055eb9efaec
Aaron <abarnard@protonmail.com>
2020-04-20 12:34:10 +1000

    Testing CI

--- src/services.js
+++ src/services.js
@@ -63,7 +63,7 @@ export function initOnboard(subscriptions) {
 
 export function initNotify() {
   const notify = staging ? stagingNotify : Notify
-  console.log('using staging notify')
+  console.log('using staging notify', staging)
   return notify({
     dappId: '12153f55-f29e-4f11-aa07-90f10da5d778',
     networkId,
