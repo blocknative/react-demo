23c318b1f66dae0fa4a91a14ea08f2a3d07bd1a9
Adam Carpenter <Adam@blocknative.com>
2021-12-15 15:21:19 -0700

    fix github hover logo, update UI settings title, include screanshot for readme

--- src/App.js
+++ src/App.js
@@ -504,7 +504,7 @@ const App = () => {
             </div>
           </div>
           <div className="container ui-settings">
-            <h3>UI Settings</h3>
+            <h3>Onboard / Notify UI Settings</h3>
             <button
               className={`bn-demo-button ${
                 darkMode ? 'selected-toggle-btn' : 'unselected-toggle-btn'
@@ -534,7 +534,7 @@ const App = () => {
           </div>
         </div>
       </section>
-      <Footer/>
+      <Footer />
     </main>
   )
 }
