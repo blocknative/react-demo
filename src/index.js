3f0689bcf7c72d0b19ef2cada27518ecf15bd593
Mahmud Adeleye <mahmud@blocknative.com>
2022-07-07 01:02:30 -0400

    react-18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
