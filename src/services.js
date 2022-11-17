b0cbc0708761c8d67fca442380a6659f5824803d
Adam Carpenter <adamcarpenter86@gmail.com>
2022-11-17 14:12:08 -0700

    Refine uauth redirectUri

--- src/services.js
+++ src/services.js
@@ -65,10 +65,10 @@ const web3auth = web3authModule({
   clientId:
     'DJuUOKvmNnlzy6ruVgeWYWIMKLRyYtjYa9Y10VCeJzWZcygDlrYLyXsBQjpJ2hxlBO9dnl8t9GmAC2qOP5vnIGo'
 })
-const windowUrl = window.location.href
+
 const uauthOptions = {
-  clientID: 'a25c3a65-a1f2-46cc-a515-a46fe7acb78c',
-  redirectUri: 'https://staging.reactdemo.blocknative.com/',
+  clientID: '2d14b025-cb94-44b9-85ac-ce2397e6f10b',
+  redirectUri: window.location.href,
   scope:
     'openid wallet email:optional humanity_check:optional profile:optional social:optional'
 }
