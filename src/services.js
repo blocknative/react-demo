6f3813939176544d57af40081f3a57b510e35df9
Adam Carpenter <adamcarpenter86@gmail.com>
2023-08-30 07:47:04 -0600

    Bump w3o modulse to the latest

--- src/services.js
+++ src/services.js
@@ -112,7 +112,6 @@ const trezorOptions = {
   email: 'test@test.com',
   appUrl: 'https://www.blocknative.com',
   consecutiveEmptyAccountThreshold: 10
-  // containerElement: '#sample-container-el'
 }
 const trezor = trezorModule(trezorOptions)
 
